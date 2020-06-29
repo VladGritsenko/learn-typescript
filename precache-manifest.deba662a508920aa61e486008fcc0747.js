af8a2c921612e70354bfde023a7d2d6dbb571014
Vlad Grytsenko <vladik.grytsenko2017@gmail.com>
2020-06-30 00:18:01 +0300

    Updates

---
+++ precache-manifest.deba662a508920aa61e486008fcc0747.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e47e87ab4d47cc3127168547660031",
+    "url": "/learn-typescript/index.html"
+  },
+  {
+    "revision": "042ce3ff745f63604eed",
+    "url": "/learn-typescript/static/css/main.41160809.chunk.css"
+  },
+  {
+    "revision": "ac0233860aa87fa1b9c8",
+    "url": "/learn-typescript/static/js/2.85c3c3f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learn-typescript/static/js/2.85c3c3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042ce3ff745f63604eed",
+    "url": "/learn-typescript/static/js/main.d4fc54cc.chunk.js"
+  },
+  {
+    "revision": "f5f4fb38812fb05d8825",
+    "url": "/learn-typescript/static/js/runtime-main.b6d0a7e4.js"
+  },
+  {
+    "revision": "81da6a92a58c015ddd0a9c98dd3a7d95",
+    "url": "/learn-typescript/static/media/facebook.81da6a92.svg"
+  },
+  {
+    "revision": "42530325aecb12bcae4c9b27d2d140b6",
+    "url": "/learn-typescript/static/media/instagram.42530325.svg"
+  },
+  {
+    "revision": "401e6c40ce48cf92cfbf3f36d911674f",
+    "url": "/learn-typescript/static/media/linkedin.401e6c40.svg"
+  },
+  {
+    "revision": "a8073dabd5ed80c4f90cfcb9497f4c63",
+    "url": "/learn-typescript/static/media/mail.a8073dab.svg"
+  },
+  {
+    "revision": "33faa92a8f9980389fb7d9440c107d34",
+    "url": "/learn-typescript/static/media/mainPhoto.33faa92a.jpg"
+  },
+  {
+    "revision": "eaaa42b19fc32b6903d88f82c99a5788",
+    "url": "/learn-typescript/static/media/react_native.eaaa42b1.svg"
+  }
+]);
